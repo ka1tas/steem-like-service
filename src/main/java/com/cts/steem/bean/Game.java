1f17f579890a7240610c63d4fad102f4503e2450
Saikat Singha Mahapatra <saikat.singhamahapatra@cognizant.com>
2018-11-20 17:34:46 +0530

    new bwan game

---
+++ src/main/java/com/cts/steem/bean/Game.java
@@ -0,0 +1,141 @@
+package com.cts.steem.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="game")
+public class Game {
+	private int id;
+	private String name;
+	private String developer;
+	private String description;
+	private String about;
+	private int price;
+	private int rating;
+	private String genre;
+	private int year;
+	
+	
+	public Game(int id, String name, String developer, String description, String about, int price, int rating,
+			String genre, int year) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.developer = developer;
+		this.description = description;
+		this.about = about;
+		this.price = price;
+		this.rating = rating;
+		this.genre = genre;
+		this.year = year;
+	}
+
+
+	public Game() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getAbout() {
+		return about;
+	}
+
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getRating() {
+		return rating;
+	}
+
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", developer=" + developer + ", description=" + description
+				+ ", about=" + about + ", price=" + price + ", rating=" + rating + ", genre=" + genre + ", year=" + year
+				+ "]";
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/cts/steem/bean/User.java
+++ src/main/java/com/cts/steem/bean/User.java
@@ -5,8 +5,199 @@ public class User {
 	private int id;
 	private String firstName;
 	private String lastName;
+	private String gender; 
+	private int age;
+	private String country;
+	private String email;
+	private int mobileno;
+	private String description;
+	private int steempoints; 
 	private String userName;
 	private String password;
+	
+	
+	
+	public User(int id, String firstName, String lastName, String gender, int age, String country, String email,
+			int mobileno, String description, int steempoints, String userName, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.age = age;
+		this.country = country;
+		this.email = email;
+		this.mobileno = mobileno;
+		this.description = description;
+		this.steempoints = steempoints;
+		this.userName = userName;
+		this.password = password;
+	}
+
+
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public int getMobileno() {
+		return mobileno;
+	}
+
+
+
+	public void setMobileno(int mobileno) {
+		this.mobileno = mobileno;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public int getSteempoints() {
+		return steempoints;
+	}
+
+
+
+	public void setSteempoints(int steempoints) {
+		this.steempoints = steempoints;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", gender=" + gender
+				+ ", age=" + age + ", country=" + country + ", email=" + email + ", mobileno=" + mobileno
+				+ ", description=" + description + ", steempoints=" + steempoints + ", userName=" + userName
+				+ ", password=" + password + "]";
+	}
+	
+	
+	
+	
 	
 
 }
